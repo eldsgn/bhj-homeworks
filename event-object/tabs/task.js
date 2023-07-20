61eb4de313da9ab9d0b7dc047a713d3f4c932fe1
Sergey Kozlov <eldsgn@gmail.com>
2023-07-20 12:26:27 +0300

    first commit tabs

---
+++ event-object/tabs/task.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const tabs = document.querySelectorAll('.tab');
+    const tabContents = document.querySelectorAll('.tab__content');
+
+    tabs.forEach((tab, index) => {
+      tab.addEventListener('click', function() {
+        tabs.forEach((tab) => tab.classList.remove('tab_active'));
+        tabContents.forEach((content) => content.classList.remove('tab__content_active'));
+
+        tab.classList.add('tab_active');
+        tabContents[index].classList.add('tab__content_active');
+      });
+    });
+  });
+  
